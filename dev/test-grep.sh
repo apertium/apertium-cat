77ecb277cf599ada29b18a94080b0d80a6dd3318
Jaume Ortolà <jaumeortola@gmail.com>
2020-05-07 14:12:35 +0200

    Update test-grep.sh

--- dev/test-grep.sh
+++ dev/test-grep.sh
@@ -20,7 +20,7 @@ do
     #Poden ser correctes
     grep -H "<b/></l>" $file | sed -E '/n="(~|cont)/d'
     grep -H "<b/></r>" $file | sed -E '/n="(~|cont)/d'
-    grep -H "<b/></i>" $file | sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__relecho "")/d' 
+    grep -H "<b/></i>" $file | sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__rel)/d' 
     
 done
 } > "greptests.txt"
