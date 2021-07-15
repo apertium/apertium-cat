78c3f44bdbdf3c2809db445f813224827d317ef1
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-07-15 15:45:22 -0500

    convert existing `make test` to apertium-regtest

---
+++ test/test-grep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for file in apertium-cat.cat.metadix
+do 
+    grep -H "<i> " $file
+    grep -H " </i>" $file
+    grep -H "<l> " $file
+    grep -H " </l>" $file
+    grep -H "<r> " $file
+    grep -H " </r>" $file
+    grep -H -P "\xa0" $file # non-breaking space
+    grep -H "<b/><s" $file
+    grep -H "<b/><b/>" $file
+    grep -H "<b/><g>" $file
+    grep -H -P "<i>[^<]+ [^<]+</i>" $file
+    grep -H -P "<l>[^<]+ [^<]+</l>" $file
+    grep -H -P "<r>[^<]+ [^<]+</r>" $file
+
+    #Poden ser correctes
+    grep -H "<b/></l>" $file | sed -E '/n="(~|cont)/d'
+    grep -H "<b/></r>" $file | sed -E '/n="(~|cont)/d'
+    grep -H "<b/></i>" $file | sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__rel)/d' 
+    
+done
