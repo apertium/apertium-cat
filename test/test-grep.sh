797412ffb716f876277f152b1a0eca7373c293e1
Marc Riera <marc.riera.irigoyen@gmail.com>
2025-03-16 13:02:08 +0100

    Update grep test

--- test/test-grep.sh
+++ test/test-grep.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+#set -e
+#set -o pipefail
+echo "Grep tests"
+{
 for file in apertium-cat.cat.metadix
 do 
-    grep -H "<i> " $file
-    grep -H " </i>" $file
-    grep -H "<l> " $file
-    grep -H " </l>" $file
-    grep -H "<r> " $file
-    grep -H " </r>" $file
     grep -H -P "\xa0" $file # non-breaking space
-    grep -H "<b/><s" $file
-    grep -H "<b/><b/>" $file
-    grep -H "<b/><g>" $file
     grep -H -P "<i>[^<]+ [^<]+</i>" $file
     grep -H -P "<l>[^<]+ [^<]+</l>" $file
     grep -H -P "<r>[^<]+ [^<]+</r>" $file
-
-    #Poden ser correctes
-    grep -H "<b/></l>" $file | sed -E '/n="(~|cont)/d'
-    grep -H "<b/></r>" $file | sed -E '/n="(~|cont)/d'
-    grep -H "<b/></i>" $file | sed -E '/n="(~|cont|romana|.*el_qual__rel|.*el_seu__rel)/d' 
-    
 done
+
+for file in $(mktemp)
+do
+    lt-expand apertium-cat.cat.metadix > $file
+    grep " :" $file
+    grep ": " $file
+    grep " <" $file
+    grep "  " $file
+    grep " #" $file
+    grep -P "^ " $file
+    rm $file
+done
+}
